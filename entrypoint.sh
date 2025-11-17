74b485d7cf130a8ada12fab0409912a66ea5ae1d
vojo28 <vojo28@gmail.com>
2025-11-17 09:52:06 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Fix file permissions so PHP can read the secret
-if [ -f /etc/secrets/credentials.json ]; then
-    chmod 644 /etc/secrets/credentials.json
-fi
-
 # Start Apache
 apache2-foreground
