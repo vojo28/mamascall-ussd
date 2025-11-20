25da68ef4eec96a6575be6f307714ce9c59d9748
vojo28 <vojo28@gmail.com>
2025-11-20 11:02:42 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+# Fix permissions so PHP/Apache can read the Google credentials
+if [ -f /etc/secrets/credentials.json ]; then
+    chmod 644 /etc/secrets/credentials.json
+fi
+
 # Start Apache
 apache2-foreground
